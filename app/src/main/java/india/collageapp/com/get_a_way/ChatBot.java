27dbfdd548c418180bd92980ffd78bf7f32ca482
LisaThomas9 <lisasarahthomas@gmail.com>
2016-03-22 23:44:42 +0530

    ChatBot related activities

---
+++ app/src/main/java/india/collageapp/com/get_a_way/ChatBot.java
@@ -0,0 +1,139 @@
+package india.collageapp.com.get_a_way;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class ChatBot extends AppCompatActivity implements LocationListener {
+
+    double mLatitude = 0;
+    double mLongitude = 0;
+    String PLACES_SEARCH_URL="https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_bot);
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+        // Getting the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+        // Getting Current Location From GPS
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+            onLocationChanged(location);
+        }
+        locationManager.requestLocationUpdates(provider, 20000, 0, this);
+
+        TextView lat = (TextView)findViewById(R.id.lat);
+        TextView lng = (TextView)findViewById(R.id.lng);
+        lat.setText("Latitude: " + String.valueOf(mLatitude) + "\nLongitude: " + String.valueOf(mLongitude));
+
+        StringBuilder sb = new StringBuilder(PLACES_SEARCH_URL);
+        sb.append("location="+mLatitude+","+mLongitude);
+        sb.append("&radius=5000");
+        sb.append("&types=shopping_mall");
+        sb.append("&sensor=true");
+        sb.append("&key=AIzaSyC1PUYZSljcphlf0zn2rN_Ae2MP8MWVlfU");
+
+        PlacesTask placesTask = new PlacesTask();
+        placesTask.execute(sb.toString());
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLatitude = location.getLatitude();
+        mLongitude = location.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    private String downloadUrl(String theUrl) {
+        StringBuilder content = new StringBuilder();
+
+        try {
+            URL url = new URL(theUrl);
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()), 8);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("DATA",content.toString());
+        return content.toString();
+    }
+
+
+    private class PlacesTask extends AsyncTask<String, Integer, String>
+    {
+
+        String data = null;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(String... url) {
+            try {
+                Log.d("LINK",url[0]);
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String result)
+        {
+            Intent intent = new Intent(getBaseContext(), ChatBotMaps.class);
+            intent.putExtra("placeList", result);
+            startActivity(intent);
+
+        }
+
+    }
+}
+
