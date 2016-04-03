48c259c9ab7a9811cf4e7c5b99ce4e8a040fcd8d
LisaThomas9 <lisasarahthomas@gmail.com>
2016-04-03 17:36:58 +0530

    Chat bot questions uploaded

--- app/src/main/java/india/collageapp/com/get_a_way/ChatBot.java
+++ app/src/main/java/india/collageapp/com/get_a_way/ChatBot.java
@@ -14,6 +14,8 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,38 +23,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Calendar;
 
 
 public class ChatBot extends AppCompatActivity implements LocationListener {
 
     double mLatitude = 0;
     double mLongitude = 0;
+    String[] questions = new String[3];
+    String[] answers = new String[3];
+    int i =0;
+    TextView t1;
+    EditText e;
+    Button b;
+    String ans;
+
     String PLACES_SEARCH_URL="https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot);
-/*  // Lisa version
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        // Creating a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
-        // Getting the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
-        // Getting Current Location From GPS
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(provider);
 
+        t1 = (TextView)findViewById(R.id.textView1);
+        e = (EditText) findViewById(R.id.editText);
+        b = (Button)findViewById(R.id.button1);
 
-        if (location != null) {
-            onLocationChanged(location);
-        }
-        locationManager.requestLocationUpdates(provider, 20000, 0, this);
-*/
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.setEnabled(true);
+            }
+        });
 
-        // My(Vishwas) version( changed only this part ) from here
         GPSTracker gps = new GPSTracker(this);
         if(gps.canGetLocation())
         {
@@ -69,24 +73,130 @@ public class ChatBot extends AppCompatActivity implements LocationListener {
             gps.showSettingsAlert();
         }
 
-        // what I(vishwas) changed ends here
+        askQuestions();
+    }
 
-        TextView lat = (TextView)findViewById(R.id.lat);
-        TextView lng = (TextView)findViewById(R.id.lng);
-        lat.setText("Latitude: " + String.valueOf(mLatitude) + "\nLongitude: " + String.valueOf(mLongitude));
+    public void askQuestions()
+    {
+        Calendar rightNow = Calendar.getInstance();
+        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+        String greeting="";
+        Log.d("TIME", String.valueOf(hour));
+        if(hour < 12)
+            greeting = "Good Morning !!";
+        else if(hour < 16)
+            greeting = "Good Afternoon !!";
+        else
+            greeting = "Good Evening !!";
 
+
+        String name = "Sharvel";
+
+
+        questions[0] = greeting+"\nHey "+name+" !!! Where would you like to go ?";
+        questions[1] = "How far do you want to travel ? (in Km)";
+        questions[2] = "How much time do you have at hand ?";
+
+        t1.setText(questions[0]);
+    }
+
+    public void getReply(View v)
+    {
+        boolean flag;
+        b.setEnabled(false);
+        ans = e.getText().toString();
+        String ques = t1.getText().toString();
+        i = Arrays.asList(questions).indexOf(ques);
+        Log.d("INDEX",String.valueOf(i));
+        if(ans == "")
+        {
+            flag = false;
+        }
+        flag = preprocess();
+        e.setText("");
+        if(! flag)
+        {
+            //make tooltip appear
+        }
+        else {
+            if(i != 2)
+            {
+                ++i;
+                getNext();
+            }
+            else
+            {
+                e.setEnabled(false);
+                t1.setText("Locating nearby "+answers[0]+" for you ");
+                formQueryString();
+            }
+
+        }
+    }
+
+    private boolean preprocess()
+    {
+        boolean result = true;
+        if(i==0)
+        {
+            answers[i] = ans;
+            //TODO
+        }
+        if(i == 1 || i == 2)
+        {
+            boolean check = isInteger(ans);
+            if(check)
+            {
+                int distance = Integer.parseInt(ans);
+                if(distance != 0) {
+                    distance *= 1000; // km into metres
+                    answers[i] = String.valueOf(distance);
+                }
+                else
+                {
+                    //enter non zero distance
+                    result = false;
+                }
+
+            }
+            else{
+                //not an integer
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        // only got here if we didn't return false
+        return true;
+    }
+
+    public void getNext()
+
+    {
+        t1.setText(questions[i]);
+    }
+
+    public void formQueryString()
+    {
         StringBuilder sb = new StringBuilder(PLACES_SEARCH_URL);
         sb.append("location="+mLatitude+","+mLongitude);
-        sb.append("&radius=5000");
-        sb.append("&types=shopping_mall");
+        sb.append("&radius="+answers[1]);
+        sb.append("&types="+answers[0]);
         sb.append("&sensor=true");
         sb.append("&key=AIzaSyC1PUYZSljcphlf0zn2rN_Ae2MP8MWVlfU");
 
         PlacesTask placesTask = new PlacesTask();
         placesTask.execute(sb.toString());
-
     }
-
     @Override
     public void onLocationChanged(Location location) {
         mLatitude = location.getLatitude();
